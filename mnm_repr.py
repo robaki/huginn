3285024a94d9f9d548e86afa91deff61cf809925
robaki <rozanskirobert@gmail.com>
2014-11-25 22:20:45 +0000

    next portion of tests for the MNM representation: properties, conditions, interventions and models

--- mnm_repr.py
+++ mnm_repr.py
@@ -34,43 +34,44 @@ class Complex(Entity):
 
 
 class Activity(Element):
-	def __init__(self):
+	def __init__(self, ID, name):
+		Element.__init__(self, ID, name)
 		self.detection_cost = None
 
 class Growth(Activity):
-	def __init__(self, conditions):
-		Activity.__init__(self)
-		self.required_conditions = conditions
+	def __init__(self, conditions, ID, name=None):
+		Activity.__init__(self, ID, name)
+		self.required_conditions = [x for x in conditions]
 
 class Expression(Activity):
-	def __init__(self, coding_gene, product_conditions):
-		Activity.__init__(self)
+	def __init__(self, coding_gene, product_conditions, ID, name=None):
+		Activity.__init__(self, ID, name)
 		self.coding_gene = coding_gene
-		self.product_conditions = product_conditions
+		self.product_conditions = [x for x in product_conditions]
 
 class NonEnzymaticReaction(Activity):
-	def __init__(self, substrates, products):
-		Activity.__init__(self)
-		self.substrates = substrates
-		self.products = products
+	def __init__(self, substrates, products, ID, name=None):
+		Activity.__init__(self, ID, name)
+		self.substrates = [x for x in substrates]
+		self.products = [x for x in products]
 
 class EnzymaticReaction(Activity):
-	def __init__(self, substrates, products):
-		Activity.__init__(self)
-		self.substrates = substrates
-		self.products = products
+	def __init__(self, substrates, products, ID, name=None):
+		Activity.__init__(self, ID, name)
+		self.substrates = [x for x in substrates]
+		self.products = [x for x in products]
 
 class TransporterNotRequired(Activity):
-	def __init__(self, source_conditions, destination_conditions):
-		Activity.__init__(self)
-		self.source_conditions = source_conditions
-		self.destination_conditions = destination_conditions
+	def __init__(self, source_conditions, destination_conditions, ID, name=None):
+		Activity.__init__(self, ID, name)
+		self.source_conditions = [x for x in source_conditions]
+		self.destination_conditions = [x for x in destination_conditions]
 
 class TransporterRequired(Activity):
-	def __init__(self, source_conditions, destination_conditions, transporter_location):
-		Activity.__init__(self)
-		self.source_conditions = source_conditions
-		self.destination_conditions = destination_conditions
+	def __init__(self, source_conditions, destination_conditions, transporter_location, ID, name=None):
+		Activity.__init__(self, ID, name)
+		self.source_conditions = [x for x in source_conditions]
+		self.destination_conditions = [x for x in destination_conditions]
 		self.transporter_location = transporter_location
 
 
@@ -88,6 +89,8 @@ class Transports(Property):
 
 
 class Condition:
+	def __init__(self):
+		pass
 
 class Present(Condition):
 	def __init__(self, entity, compartment):
@@ -96,39 +99,52 @@ class Present(Condition):
 
 
 class Compartment:
+	def __init__(self):
+		pass
 
 class Medium(Compartment):
+	def __init__(self):
+		pass
 
 class MediumCytosolMembrane(Compartment):
+	def __init__(self):
+		pass
 
 class Mitochondrium(Compartment):
+	def __init__(self):
+		pass
 
 class MitochMatrix(Compartment):
+	def __init__(self):
+		pass
 
 class MitochOuterMembrane(Compartment):
+	def __init__(self):
+		pass
 
 class MitochInnerMembrane(Compartment):
+	def __init__(self):
+		pass
 
-
-class Intervention():
+class Intervention:
 	def __init__(self, condition_or_activity):
 		self.condition_or_activity = condition_or_activity
 
 class Add(Intervention):
 	def __init__(self, condition_or_activity):
-		Intervention__init__(self, condition_or_activity)
+		Intervention.__init__(self, condition_or_activity)
 
 class Remove(Intervention):
 	def __init__(self, condition_or_activity):
-		Intervention__init__(self, condition_or_activity)
+		Intervention.__init__(self, condition_or_activity)
 
 
 class Model:
 	def __init__(self, ID, setup_conds, intermediate_activities, termination_conds, status='Active'):
 		self.ID = ID
-		self.setup_conds = setup_conds
-		self.intermediate_activities = intermediate_activities
-		self.termination_conds = termination_conds
+		self.setup_conds = [x for x in setup_conds]
+		self.intermediate_activities = [x for x in intermediate_activities]
+		self.termination_conds = [x for x in termination_conds]
 		self.status = status
 		self.quality = None
 
@@ -141,7 +157,7 @@ class Model:
 			if isinstance(intervention.condition_or_activity, Condition):
 				self.setup_conds.append(intervention.condition_or_activity)
 			elif isinstance(intervention.condition_or_activity, Activity):
-				self.termination_conds.append(intervention.condition_or_activity)
+				self.intermediate_activities.append(intervention.condition_or_activity)
 			else:
 				raise TypeError('intervention is neither condition nor activity: %s' % type(intervention.condition_or_activity))
 
@@ -149,7 +165,7 @@ class Model:
 			if isinstance(intervention.condition_or_activity, Condition):
 				self.setup_conds.remove(intervention.condition_or_activity)
 			elif isinstance(intervention.condition_or_activity, Activity):
-				self.termination_conds.remove(intervention.condition_or_activity)
+				self.intermediate_activities.remove(intervention.condition_or_activity)
 			else:
 				raise TypeError('intervention is neither condition nor activity: %s' % type(intervention.condition_or_activity))
 
--- tests/mnm_repr_test.py
+++ tests/mnm_repr_test.py
@@ -234,12 +234,196 @@ class TransporterRequiredTest(unittest.TestCase):
 		self.assertEqual(self.transporterrequired.transporter_location, 'location')
 		self.assertEqual(self.transporterrequired.ID, 'ID')
 
-#Present
 
-#Intervention
 
-#Add
+class PresentTest(unittest.TestCase):
 
-#Remove
+	def test_default_values(self):
+		self.present = mnm_repr.Present('entiy', 'compartment')
+		self.assertEqual(self.present.entity, 'entiy')
+		self.assertEqual(self.present.compartment, 'compartment')
 
-#Model
+
+class InterventionTest(unittest.TestCase):
+
+	def test_default_values(self):
+		self.intervention = mnm_repr.Intervention('cond_or_act')
+		self.assertEqual(self.intervention.condition_or_activity, 'cond_or_act')
+
+
+class AddTest(unittest.TestCase):
+
+	def test_default_values(self):
+		self.add = mnm_repr.Add('cond_or_act')
+		self.assertEqual(self.add.condition_or_activity, 'cond_or_act')
+
+
+class RemoveTest(unittest.TestCase):
+
+	def test_default_values(self):
+		self.remove = mnm_repr.Remove('cond_or_act')
+		self.assertEqual(self.remove.condition_or_activity, 'cond_or_act')
+
+
+class ModelTest(unittest.TestCase):
+
+	def test_setup_empty(self):
+		setup_conds = []
+		interm_activs = []
+		term_conds = []
+		self.model = mnm_repr.Model('ID', setup_conds, interm_activs, term_conds)
+		self.assertEqual(self.model.ID, 'ID')
+		self.assertEqual(self.model.setup_conds, setup_conds)
+		self.assertIsNot(self.model.setup_conds, setup_conds)
+		self.assertEqual(self.model.intermediate_activities, interm_activs)
+		self.assertIsNot(self.model.intermediate_activities, interm_activs)
+		self.assertEqual(self.model.termination_conds, term_conds)
+		self.assertIsNot(self.model.termination_conds, term_conds)
+
+	def test_setup_with_lists(self):
+		con1 = mnm_repr.Present('ent1','comp1')
+		con2 = mnm_repr.Present('ent2','comp2')
+		con3 = mnm_repr.Present('ent3','comp3')
+		con4 = mnm_repr.Present('ent4','comp4')
+		act1 = mnm_repr.Activity('ID1', 'name1')
+		act2 = mnm_repr.Activity('ID2', 'name2')
+		setup_conds = [con1, con2]
+		interm_activs = [act1, act2]
+		term_conds = [con3, con4]
+		self.model = mnm_repr.Model('ID', setup_conds, interm_activs, term_conds)
+		self.assertEqual(self.model.ID, 'ID')
+		self.assertEqual(self.model.setup_conds, setup_conds)
+		self.assertIsNot(self.model.setup_conds, setup_conds)
+		self.assertEqual(self.model.intermediate_activities, interm_activs)
+		self.assertIsNot(self.model.intermediate_activities, interm_activs)
+		self.assertEqual(self.model.termination_conds, term_conds)
+		self.assertIsNot(self.model.termination_conds, term_conds)
+
+
+	def test_apply_intervention_add_condition(self):
+		con1 = mnm_repr.Present('ent1','comp1')
+		con2 = mnm_repr.Present('ent2','comp2')
+		con3 = mnm_repr.Present('ent3','comp3')
+		con4 = mnm_repr.Present('ent4','comp4')
+		act1 = mnm_repr.Activity('ID1', 'name1')
+		act2 = mnm_repr.Activity('ID2', 'name2')
+		intervention = mnm_repr.Add(con1)
+		setup_conds = [con2]
+		interm_activs = [act1, act2]
+		term_conds = [con3, con4]
+		self.model = mnm_repr.Model('ID', setup_conds, interm_activs, term_conds)
+		self.model.apply_intervention(intervention)
+		self.assertEqual(self.model.setup_conds, [con2, con1])
+
+
+	def test_apply_intervention_remove_condition(self):
+		con1 = mnm_repr.Present('ent1','comp1')
+		con2 = mnm_repr.Present('ent2','comp2')
+		con3 = mnm_repr.Present('ent3','comp3')
+		con4 = mnm_repr.Present('ent4','comp4')
+		act1 = mnm_repr.Activity('ID1', 'name1')
+		act2 = mnm_repr.Activity('ID2', 'name2')
+		intervention = mnm_repr.Remove(con1)
+		setup_conds = [con1, con2]
+		interm_activs = [act1, act2]
+		term_conds = [con3, con4]
+		self.model = mnm_repr.Model('ID', setup_conds, interm_activs, term_conds)
+		self.model.apply_intervention(intervention)
+		self.assertEqual(self.model.setup_conds, [con2])
+
+
+	def test_apply_intervention_add_activity(self):
+		con1 = mnm_repr.Present('ent1','comp1')
+		con2 = mnm_repr.Present('ent2','comp2')
+		con3 = mnm_repr.Present('ent3','comp3')
+		con4 = mnm_repr.Present('ent4','comp4')
+		act1 = mnm_repr.Activity('ID1', 'name1')
+		act2 = mnm_repr.Activity('ID2', 'name2')
+		intervention = mnm_repr.Add(act1)
+		setup_conds = [con1, con2]
+		interm_activs = [act2]
+		term_conds = [con3, con4]
+		self.model = mnm_repr.Model('ID', setup_conds, interm_activs, term_conds)
+		self.model.apply_intervention(intervention)
+		self.assertEqual(self.model.intermediate_activities, [act2, act1])
+
+
+	def test_apply_intervention_remove_activity(self):
+		con1 = mnm_repr.Present('ent1','comp1')
+		con2 = mnm_repr.Present('ent2','comp2')
+		con3 = mnm_repr.Present('ent3','comp3')
+		con4 = mnm_repr.Present('ent4','comp4')
+		act1 = mnm_repr.Activity('ID1', 'name1')
+		act2 = mnm_repr.Activity('ID2', 'name2')
+		intervention = mnm_repr.Remove(act1)
+		setup_conds = [con1, con2]
+		interm_activs = [act1, act2]
+		term_conds = [con3, con4]
+		self.model = mnm_repr.Model('ID', setup_conds, interm_activs, term_conds)
+		self.model.apply_intervention(intervention)
+		self.assertEqual(self.model.intermediate_activities, [act2])
+
+
+	def test_apply_multiple_interventions(self):
+		con1 = mnm_repr.Present('ent1','comp1')
+		con2 = mnm_repr.Present('ent2','comp2')
+		con3 = mnm_repr.Present('ent3','comp3')
+		con4 = mnm_repr.Present('ent4','comp4')
+		act1 = mnm_repr.Activity('ID1', 'name1')
+		act2 = mnm_repr.Activity('ID2', 'name2')
+		intervention1 = mnm_repr.Add(con1)
+		intervention2 = mnm_repr.Remove(con1)
+		intervention3 = mnm_repr.Add(act1)
+		intervention4 = mnm_repr.Remove(act1)
+		setup_conds = [con2]
+		interm_activs = [act2]
+		term_conds = [con3, con4]
+		self.model = mnm_repr.Model('ID', setup_conds, interm_activs, term_conds)
+		self.model.apply_interventions([intervention1, intervention2, intervention3, intervention4])
+		self.assertEqual(self.model.setup_conds, [con2])
+		self.assertEqual(self.model.intermediate_activities, [act2])
+
+
+	def test_apply_intervention_raise_not_intervention(self):
+		con1 = mnm_repr.Present('ent1','comp1')
+		con2 = mnm_repr.Present('ent2','comp2')
+		con3 = mnm_repr.Present('ent3','comp3')
+		con4 = mnm_repr.Present('ent4','comp4')
+		act1 = mnm_repr.Activity('ID1', 'name1')
+		act2 = mnm_repr.Activity('ID2', 'name2')
+		intervention = 'fake intervention'
+		setup_conds = [con1, con2]
+		interm_activs = [act1, act2]
+		term_conds = [con3, con4]
+		self.model = mnm_repr.Model('ID', setup_conds, interm_activs, term_conds)
+		self.assertRaises(TypeError, self.model.apply_intervention, intervention)
+
+
+	def test_apply_intervention_raise_not_intervention(self):
+		con1 = mnm_repr.Present('ent1','comp1')
+		con2 = mnm_repr.Present('ent2','comp2')
+		con3 = mnm_repr.Present('ent3','comp3')
+		con4 = mnm_repr.Present('ent4','comp4')
+		act1 = mnm_repr.Activity('ID1', 'name1')
+		act2 = mnm_repr.Activity('ID2', 'name2')
+		intervention = 'fake intervention'
+		setup_conds = [con1, con2]
+		interm_activs = [act1, act2]
+		term_conds = [con3, con4]
+		self.model = mnm_repr.Model('ID', setup_conds, interm_activs, term_conds)
+		self.assertRaises(TypeError, self.model.apply_intervention, intervention)
+
+
+	def test_apply_intervention_raise_neither_cond_nor_activity(self):
+		con1 = mnm_repr.Present('ent1','comp1')
+		con2 = mnm_repr.Present('ent2','comp2')
+		con3 = mnm_repr.Present('ent3','comp3')
+		con4 = mnm_repr.Present('ent4','comp4')
+		act1 = mnm_repr.Activity('ID1', 'name1')
+		act2 = mnm_repr.Activity('ID2', 'name2')
+		intervention = mnm_repr.Intervention('nothing')
+		setup_conds = [con1, con2]
+		interm_activs = [act1, act2]
+		term_conds = [con3, con4]
+		self.model = mnm_repr.Model('ID', setup_conds, interm_activs, term_conds)
+		self.assertRaises(TypeError, self.model.apply_intervention, intervention)
