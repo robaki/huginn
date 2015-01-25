7c803a7b3ecc3e8eaa2d7c48e93fb0350de513bd
robaki <rozanskirobert@gmail.com>
2015-01-25 15:00:07 +0000

    cleaning up

---
+++ exporter.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+
+
+	def revision(self, weights_revision, ignoring, weights_ignoring):
+		self.weights_revision = weights_revision
+		self.ignoring = ignoring
+		self.weights_ignoring = weights_ignoring
+
+
+	def experiment_design((self, basic, advanced, secondary_criterion_cost):
+		self.basic = basic
+		self.advanced = advanced
+		self.secondary_criterion_cost = secondary_criterion_cost
